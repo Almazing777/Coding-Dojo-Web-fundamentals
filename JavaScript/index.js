0c8f7528e9cdfbce75473a6ab940d86b778e8c88
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2017-09-18 22:15:50 -0400

    Delete index.js

--- JavaScript/index.js
+++
@@ -1,8 +0,0 @@
-var names = ["James", "Jill", "Jane","Jack"];
-
-function printnames (arr, symbol){
-  for(var i=0; i<arr.length; i++){
-    console.log(i+" "+symbol + " " + names[i]);
-  }
-}
-printnames(names, "->");
